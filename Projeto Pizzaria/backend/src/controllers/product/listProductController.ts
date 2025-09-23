fa49bf47477ce5d01df42131d8442e8f84ea796a
alencarpedr <pedro.a.pinto7@aluno.senai.br>
2025-09-23 09:03:10 -0300

    :construction: Correção de erros no caminho de imagens

--- Projeto Pizzaria/backend/src/controllers/product/listProductController.ts
+++ Projeto Pizzaria/backend/src/controllers/product/listProductController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express/'
 import { ListProductByCategory } from '../../services/product/listProductService'
-// import { v2 as cloudinary } from 'cloudinary'
 
 class ListProductByCategoryController{
     async handle(req: Request, res: Response){
@@ -9,21 +8,9 @@ class ListProductByCategoryController{
             const { category_id } = req.body
 
             const listProductService = new ListProductByCategory()
-             
-            // if (!req.file) {
-            //     throw new Error("Erro no upload da imagem");
-            // }
-
-            // const file = req.file;
-
-            // const resultFile = await cloudinary.uploader.upload(file.path, {
-            //     folder: "produtos",
-            //     resource_type: "image",
-            // })
 
             const produtosPorCategoria = await listProductService.execute({
-                category_id,
-                // image_url: resultFile.secure_url
+                category_id
             });
 
             return res.json(produtosPorCategoria)
