aeddeff6f7ea2b29ed7d6797670813ac92683e06
GabrielCFreire <gabriel.c.freire6@aluno.senai.br>
2025-10-07 14:57:51 -0300

    :bug: Arrumando dependências

--- Projeto Pizzaria/mobile/src/services/api.ts
+++ Projeto Pizzaria/mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://localhost:3333
-    baseURL: 'http://10.65.105.76:3333',
+    baseURL: 'http://10.65.105.46:3333',
     timeout: 10000,
 })
 
