acdbcd11b2086b176273996441ea9b5a5fed7157
alencarpedr <pedro.a.pinto7@aluno.senai.br>
2025-10-14 16:59:47 -0300

    :arrow_up: Tornando o segundo sabor visível na tela da comanda.

--- Projeto Pizzaria/mobile/src/services/api.ts
+++ Projeto Pizzaria/mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://localhost:3333
-    baseURL: 'http://10.54.173.26:3333',
+    baseURL: 'http://10.206.49.76:3333',
     timeout: 10000,
 })
 
